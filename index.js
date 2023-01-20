081785a7a20a1d2c55ae973cdf502e005cb04d06
Malachi <46620650+MalachiVargas@users.noreply.github.com>
2023-01-20 07:23:56 +0000

    changed token to TOKEN in index

--- index.js
+++ index.js
@@ -6,38 +6,38 @@ const client = new Client({ intents: 32767 });
 
 // Array of Command objects
 const cmds = [{
-    name: `ping`, // Command name
-    description: `Ping!`, // Command description
-    async execute(interaction) { // Execute function
-        await interaction.reply({
-            content: `Pong.`
-        });
-    }
+  name: `ping`, // Command name
+  description: `Ping!`, // Command description
+  async execute(interaction) { // Execute function
+    await interaction.reply({
+      content: `Pong.`
+    });
+  }
 }]
 
 // Interaction Create Event
 client.on('interactionCreate', async interaction => {
-    if (interaction.isCommand()) {
-        await cmds.forEach(async command => {
-            if (interaction.commandName == command.name) {
-                try {
-                    await command.execute(interaction);
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-        });
-    }
+  if (interaction.isCommand()) {
+    await cmds.forEach(async command => {
+      if (interaction.commandName == command.name) {
+        try {
+          await command.execute(interaction);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
+  }
 });
 
 // Ready Event
 client.on('ready', async () => {
-    console.log(`Testing bot is now online successfully!`);
-    await client.guilds.cache
-            .get(process.env['serverId'])
-            .commands.set(cmds);
+  console.log(`Testing bot is now online successfully!`);
+  await client.guilds.cache
+    .get(process.env['serverId'])
+    .commands.set(cmds);
 });
 
 // Bot Login
-client.login(process.env['token']);
+client.login(process.env['TOKEN']);
 keepAlive();
