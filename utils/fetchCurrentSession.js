2b31aa28a74f7a1ce417ffab238a370cfedf5d18
Malachi Vargas <mcvarga7@gmail.com>
2023-02-03 05:34:19 -0500

    fixed fetch requests

--- utils/fetchCurrentSession.js
+++ utils/fetchCurrentSession.js
@@ -1,14 +1,5 @@
 const fetchCurrentSession = async (user_id) => {
-	const res = await fetch('https://SentiBot.malachivargas.repl.co/get_chatroom_ids',
-		{
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.strngify({
-				user_id,
-			}),
-		});
+	const res = await fetch(`https://SentiBot.malachivargas.repl.co/get_current_chatroom/${user_id}`);
 	const data = await res.json();
 	return data;
 };
